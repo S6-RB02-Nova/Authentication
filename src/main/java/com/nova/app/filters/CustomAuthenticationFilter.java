a3c68182a185aba3c420160c8ef1ea94d87f636c
Tony <51972581+tony1658@users.noreply.github.com>
2022-06-15 01:12:20 +0200

    Fix controller

--- src/main/java/com/nova/app/filters/CustomAuthenticationFilter.java
+++ src/main/java/com/nova/app/filters/CustomAuthenticationFilter.java
@@ -40,7 +40,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
             .withSubject(user.getUsername())
             .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
             .withIssuer(request.getRequestURL().toString())
-            .withClaim("ROLES", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+            .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
             .sign(algorithm);
 
         String refresh_token = JWT.create()
--- src/main/java/com/nova/app/filters/CustomAuthorizationFilter.java
+++ src/main/java/com/nova/app/filters/CustomAuthorizationFilter.java
@@ -3,6 +3,7 @@ package com.nova.app.filters;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,14 +23,15 @@ import java.util.Collection;
 
 import static java.util.Arrays.stream;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @Slf4j
 @NoArgsConstructor
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (!request.getServletPath().equals("/authentication/login")) {
+        if (request.getServletPath().equals("/authentication/login")) {
+            filterChain.doFilter(request, response);
+        } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
             if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 try {
@@ -39,19 +41,21 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
                     DecodedJWT decodedJWT = verifier.verify(token);
                     String username = decodedJWT.getSubject();
-                    String[] roles = decodedJWT.getClaim("ROLES").asArray(String.class);
+                    String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
 
                     Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
                     stream(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
                     UsernamePasswordAuthenticationToken authenticationToken =
                         new UsernamePasswordAuthenticationToken(username, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                } catch (Exception exception) {
-                    log.error("Error logging in: {}", exception.getMessage());
-                    response.sendError(FORBIDDEN.value());
+                    filterChain.doFilter(request, response);
+                } catch (TokenExpiredException exception) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.getWriter().write(exception.getMessage());
                 }
+            } else {
+                filterChain.doFilter(request, response);
             }
         }
-        filterChain.doFilter(request, response);
     }
 }
